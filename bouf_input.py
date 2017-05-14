fb638520656a1d4a70bfca194f2e5c7ae003b4e4
boufinkorea <boufinkorea@gmail.com>
2017-05-14 15:48:44 +0900

    20035개 영상 하나의 덩어리로...

--- bouf_input.py
+++ bouf_input.py
@@ -5,35 +5,47 @@ import matplotlib.pyplot as plt
 
 
 DS_Path = '/home/jinhkim/workspace/CubeDataset/dataset_scaled/'
+
 Img_FileName = 'img/patchImg%04d.bmp' % (0)
 Depth_FileName = 'depth_txt/patchDepth%04d.txt' % (0)
-print(DS_Path+Img_FileName)
+print(DS_Path + Img_FileName)
 
-imgMat = cv2.imread(DS_Path+Img_FileName)
+imgMat = cv2.imread(DS_Path + Img_FileName)
+dataset = numpy.zeros((20035,imgMat.shape[0], imgMat.shape[1], 4),dtype=numpy.float)
 
-depth_txt = open(DS_Path+Depth_FileName, 'r')
-s = depth_txt.readline()
-l = (s.split())
-row = int(l[0])
-col = int(l[1])
-print(row, col)
-depthMat = numpy.zeros((int(row),int(col)),dtype=numpy.float)
 
-for i in range(0,row):
+
+for k in range(0,20035):
+    Img_FileName = 'img/patchImg%04d.bmp' % (k)
+    Depth_FileName = 'depth_txt/patchDepth%04d.txt' % (k)
+    print(DS_Path+Img_FileName)
+
+    imgMat = cv2.imread(DS_Path+Img_FileName)
+
+    depth_txt = open(DS_Path+Depth_FileName, 'r')
     s = depth_txt.readline()
-    l = s.split()
-    for j in range(0,col):
-        depthMat[i,j] = float(l[j])
-depth_txt.close();
+    l = (s.split())
+    row = int(l[0])
+    col = int(l[1])
+    print(row, col)
+    depthMat = numpy.zeros((int(row),int(col)),dtype=numpy.float)
+
+    for i in range(0,row):
+        s = depth_txt.readline()
+        l = s.split()
+        for j in range(0,col):
+            depthMat[i,j] = float(l[j])
+    depth_txt.close();
 
 
-inPatch = numpy.zeros((row,col,4),dtype=numpy.float)
+    inPatch = numpy.zeros((row,col,4),dtype=numpy.float)
 
-for i in range(0,row):
-    for j in range(0,col):
-        inPatch[i, j, 0] = float(imgMat[i, j, 0])
-        inPatch[i, j, 1] = float(imgMat[i, j, 1])
-        inPatch[i, j, 2] = float(imgMat[i, j, 2])
-        inPatch[i, j, 3] = depthMat[i,j]
+    for i in range(0,row):
+        for j in range(0,col):
+            inPatch[i, j, 0] = float(imgMat[i, j, 0])
+            inPatch[i, j, 1] = float(imgMat[i, j, 1])
+            inPatch[i, j, 2] = float(imgMat[i, j, 2])
+            inPatch[i, j, 3] = depthMat[i,j]
+    dataset[k] = inPatch
 
-print(inPatch.size)
+print(dataset.shape)
